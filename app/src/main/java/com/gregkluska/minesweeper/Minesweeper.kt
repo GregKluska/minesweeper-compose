bbde69031178c7a9dee7407dfe1e406b574baabb
Greg Kluska <kluskagreg@gmail.com>
2023-04-13 22:23:27 +0100

    Create game class

--- app/src/main/java/com/gregkluska/minesweeper/Minesweeper.kt
+++ app/src/main/java/com/gregkluska/minesweeper/Minesweeper.kt
@@ -1,31 +1,38 @@
 package com.gregkluska.minesweeper
 
-import kotlin.random.Random
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-fun main() {
-    val game = Minesweeper(10, 10, 30)
+//fun main() {
+//    val game = Minesweeper(10, 10, 10)
+//
+//    while(!game.gameOver) {
+//        game.printGame()
+//        print("X: ")
+//        val x = readLine()!!
+//        println()
+//        print("Y: ")
+//        val y = readLine()!!
+//        game.reveal(x.toInt(), y.toInt())
+//    }
+//}
 
-    while(!game.gameOver) {
-        game.printGame()
-        print("X: ")
-        val x = readLine()!!
-        println()
-        print("Y: ")
-        val y = readLine()!!
-        game.reveal(x.toInt(), y.toInt())
-    }
-}
+data class Field(
+    val value: Int = 0,
+    val isRevealed: Boolean = false,
+    val isFlagged: Boolean = false
+)
 
 class Minesweeper(
     val width: Int,
     val height: Int,
     val mines: Int,
 ) {
+    val board = List(height) { List(width) { mutableStateOf(Field()) } }
 
-    val board = List(height) { MutableList(width) { 0 } }
-    val flags = mutableListOf<Pair<Int, Int>>()
-    val revealed = mutableListOf<Pair<Int, Int>>()
-    var gameOver = false
+    var gameOver by mutableStateOf(false)
+        private set
 
     init {
         require(width > 0 && height > 0 && mines > 0 && width * height > mines)
@@ -35,7 +42,7 @@ class Minesweeper(
             if (mineCoords[i] == 0) continue
             val y = i.div(width)
             val x = i - (y * width)
-            board[y][x] = -1
+            board[y][x].value = board[y][x].value.copy(value = -1)
             updateAdjacentMines(x, y)
         }
 
@@ -44,15 +51,16 @@ class Minesweeper(
 
     fun reveal(x: Int, y: Int) {
         if (gameOver) return
-        if ((x to y) in flags || (x to y) in revealed) return
-        if (board[y][x] == -1) {
+        val field = board[y][x].value
+        if (field.isRevealed || field.isFlagged) return
+        if (field.value == -1) {
             gameOver = true
             return
         }
 
-        revealed.add((x to y))
+        board[y][x].value = field.copy(isRevealed = true)
 
-        if (board[y][x] == 0) {
+        if (field.value == 0) {
             for (zx in -1..1) {
                 for (zy in -1..1) {
                     if (zx == 0 && zy == 0) continue
@@ -75,34 +83,35 @@ class Minesweeper(
                 if (fx == 0 && fy == 0) continue
                 if (y + fy in 0 until height &&
                     x + fx in 0 until width &&
-                    board[y + fy][x + fx] > -1
+                    board[y + fy][x + fx].value.value > -1
                 ) {
-                    board[y + fy][x + fx]++
+                    val field = board[y + fy][x + fx].value
+                    board[y + fy][x + fx].value = field.copy(value = field.value + 1)
                 }
             }
         }
     }
 
-    fun printGame() {
-        board.forEachIndexed { y, row ->
-            row.forEachIndexed { x, num ->
-                if((x to y) in revealed) {
-                    print(if (num < 0) " X " else " $num ")
-                } else {
-                    print(" * ")
-                }
-
-            }
-            println()
-        }
-    }
-
-    private fun printBoard() {
-        board.forEach {
-            it.forEach {
-                print(if (it < 0) " X " else " $it ")
-            }
-            println()
-        }
-    }
+//    fun printGame() {
+//        board.forEachIndexed { y, row ->
+//            row.forEachIndexed { x, num ->
+//                if((x to y) in revealed) {
+//                    print(if (num.value < 0) " X " else " $num ")
+//                } else {
+//                    print(" * ")
+//                }
+//
+//            }
+//            println()
+//        }
+//    }
+//
+//    private fun printBoard() {
+//        board.forEach {
+//            it.forEach {
+//                print(if (it.value < 0) " X " else " ${it.value} ")
+//            }
+//            println()
+//        }
+//    }
 }
