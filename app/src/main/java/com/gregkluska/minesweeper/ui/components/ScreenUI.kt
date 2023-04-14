3585907ba02e94f92d82f9ecd3eb5095e0af35c4
Greg Kluska <kluskagreg@gmail.com>
2023-04-15 00:18:11 +0100

    Build UI

--- app/src/main/java/com/gregkluska/minesweeper/ui/components/ScreenUI.kt
+++ app/src/main/java/com/gregkluska/minesweeper/ui/components/ScreenUI.kt
@@ -1,8 +1,43 @@
 package com.gregkluska.minesweeper.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
+import androidx.compose.ui.unit.dp
+import com.gregkluska.minesweeper.ui.theme.MinesweeperTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScreenUI() {
-    
+fun ScreenUI(
+    content: @Composable (PaddingValues) -> Unit
+) {
+
+    MinesweeperTheme {
+        Scaffold(
+            modifier = Modifier.background(MaterialTheme.colorScheme.background),
+            topBar = { TopBar() },
+            bottomBar = { BottomBar() },
+            floatingActionButton = {},
+            content = content
+        )
+    }
+
+}
+
+@Preview(wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
+@Composable
+fun ScreenUIPreview() {
+    ScreenUI {
+        Box(
+            modifier = Modifier.size(300.dp)
+        )
+    }
 }
