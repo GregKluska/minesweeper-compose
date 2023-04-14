9229d4e3354a52cd3ed209d5c0325a2fbe314fe3
Greg Kluska <gregkluska@gmail.com>
2023-04-14 22:49:54 +0100

    Clean the code and bump versions

---
+++ app/src/main/java/com/gregkluska/minesweeper/GameScreen.kt
@@ -0,0 +1,15 @@
+package com.gregkluska.minesweeper
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GameScreen() {
+
+}
+
+@Preview
+@Composable
+fun GameScreenPreview() {
+
+}
---
+++ app/src/main/java/com/gregkluska/minesweeper/ui/components/ScreenUI.kt
@@ -0,0 +1,8 @@
+package com.gregkluska.minesweeper.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScreenUI() {
+    
+}
