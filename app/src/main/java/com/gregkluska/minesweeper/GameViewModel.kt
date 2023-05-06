cbe2a745f462acfeaa78f62440c27df547c68414
Greg Kluska <kluskagreg@gmail.com>
2023-05-06 18:27:52 +0100

    Update colors and fix bug

--- app/src/main/java/com/gregkluska/minesweeper/GameViewModel.kt
+++ app/src/main/java/com/gregkluska/minesweeper/GameViewModel.kt
@@ -26,8 +26,8 @@ class GameViewModel() : ViewModel() {
     private val viewModelGameState = mutableStateOf(
         GameState(
             game = Minesweeper(
-                width = 10,
-                height = 10,
+                width = 20,
+                height = 40,
                 mines = 10,
             ),
             flagMode = false,
